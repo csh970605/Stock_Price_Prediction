f56623ebc96640c0964203bb885cab6294678b38
Choi_Se_Hoon <csh970605@gmail.com>
2021-06-01 19:03:46 +0900

    Add files via upload

---
+++ stock_prediction2/main.py
@@ -0,0 +1,6 @@
+from stockweb import WebApplication
+
+if __name__ == '__main__':
+    web = WebApplication()
+    web.get_input()
+# streamlit run "C:/Users/csh97/PycharmProjects/stock_prediction2/main.py"
